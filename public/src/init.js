1bb1a2acf74cb140de773a7375c451533411351b
Cristhian Amaya <cristhiana@google.com>
2023-01-15 00:50:24 -0500

    Initial setup init script

---
+++ public/src/init.js
@@ -0,0 +1,5 @@
+import nmbrs from './nmbr.js'
+
+(function init() {
+    console.log('INICIALIZANDO LA APP', nmbrs);
+})()
---
+++ public/src/nmbr.js
@@ -0,0 +1,32 @@
+export default {
+    0: {
+        color: 'lightgray'
+    },
+    1: {
+        color: 'brown'
+    },
+    2: {
+        color: 'orange'
+    },
+    3: {
+        color: 'yellow'
+    },
+    4: {
+        color: 'green'
+    },
+    5: {
+        color: 'cyan'
+    },
+    6: {
+        color: 'blue'
+    },
+    7: {
+        color: 'purple'
+    },
+    8: {
+        color: 'pink'
+    },
+    9: {
+        color: 'red'
+    },
+}
